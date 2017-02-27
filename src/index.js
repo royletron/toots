772eeee4a75cf7814904e30689ae381ece0792d8
Darren Royle <darren@diffblue.com>
2017-02-27 20:36:10 +0000

    Adding reauth on firebase

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { authSuccess } from './actions/Auth';
 import { AppContainer } from 'react-hot-loader';
 import { Provider } from 'react-redux';
 import firebase from 'firebase';
 firebase.initializeApp({
   apiKey: 'AIzaSyDTvJCEgcOPQwEm-PEWwmSEIRqWO7w9xUc',
   authDomain: 'toots-fcea3.firebaseapp.com'
-});
+})
+;
 import store from './store';
+
+firebase.auth().onAuthStateChanged((user) => {
+  if(user) {
+    store.dispatch(authSuccess(user))
+  }
+})
 
 import App from './containers/App';
 
